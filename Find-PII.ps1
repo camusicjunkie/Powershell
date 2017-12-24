8a9c5bbf75c532fbb914920c4609f9176987a738
camusicjunkie <33888807+camusicjunkie@users.noreply.github.com>
2017-12-24 00:21:01 -0700

    Delete Find-PII.ps1

--- Find-PII.ps1
+++
@@ -1,9 +0,0 @@
-﻿$path
-
-
-Rename-Item -Path .\Resume.docx -NewName Resume.zip
-Expand-Archive .\Resume.zip
-[xml]$docx = Get-Content -Path .\Resume\word\document.xml
-$docx
-$patternSocial = '(\d{3}[-| ]\d{2}[-| ]\d{4})|(\d{9})'
-$docx.document.body.p.r.t | Select-String -Pattern $patternSocial -Quiet
